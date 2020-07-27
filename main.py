2b8c910f34e88bbd644e9f62f27151deb4571bda
cadasmeq (win) <cadasmeq@gmail.com>
2020-07-27 15:23:16 -0400

    Hello world app

--- main.py
+++ main.py
@@ -1,2 +1,7 @@
 from flask import Flask
 
+app = Flask(__name__)           # Crea instancia de Flask, cuyo parametro __name__ (nombre del archivo) será el nombre de la app.
+
+@app.route('/')    # Ruta donde se correrá esta función. Es decir, Cuando el buscador haga una petición a nuestro servidor, va a llegar a ruta raiz.
+def hello():
+    return "hello world Flask"
