6372c6f13d3b9c2d717098634a0502dfa6971041
cadasmeq (win) <cadasmeq@gmail.com>
2020-07-27 15:51:48 -0400

    create a cookie and redirect to other route

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
-from flask import Flask, request
+from flask import Flask, request, make_response, redirect
 
 app = Flask(__name__)           # Crea instancia de Flask, cuyo parametro __name__ (nombre del archivo) será el nombre de la app.
 
-@app.route('/')    # Ruta donde se correrá esta función. Es decir, Cuando el buscador haga una petición a nuestro servidor, va a llegar a ruta raiz.
-def hello():
+@app.route('/') # Ruta donde se correrá esta función. Es decir, Cuando el buscador haga una petición a nuestro servidor, va a llegar a ruta raiz.
+def index():
     user_ip = request.remote_addr   #obtiene la ip de la conexión remota
+    make_redir = redirect("/hello")
+    response = make_response(make_redir)
+    response.set_cookie('user_ip', user_ip)
+
+    return response
+
+
+@app.route('/hello')    
+def hello():
+    user_ip = request.cookies.get('user_ip')
     return "hello world Flask. your ip is {}".format(user_ip)
 
 
