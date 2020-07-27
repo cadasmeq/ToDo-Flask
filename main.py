4b7aae8ac76f2922cc287f768f43ebf30b65cb2e
cadasmeq (win) <cadasmeq@gmail.com>
2020-07-27 15:32:03 -0400

    flask debug on

--- main.py
+++ main.py
@@ -4,4 +4,7 @@ app = Flask(__name__)           # Crea instancia de Flask, cuyo parametro __name
 
 @app.route('/')    # Ruta donde se correrá esta función. Es decir, Cuando el buscador haga una petición a nuestro servidor, va a llegar a ruta raiz.
 def hello():
-    return "hello world Flask"
+    return "hello world, Flask"
+
+# set FLASK_APP=main.py     -> establece la variable FLASK_APP a main.py
+# set FLASK_DEBUG=1         -> establece la variable FLASK_DEBUGG a 1 para habilitar debug mode
