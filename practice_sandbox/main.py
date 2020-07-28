eeb6252120eb82c14a25f02f571deda03c7307f9
cadasmeq (linux) <cadasmeq02@gmail.com>
2020-07-28 12:31:21 -0400

    Added structure control in myip.html

--- practice_sandbox/main.py
+++ practice_sandbox/main.py
@@ -1,6 +1,8 @@
 from flask import Flask, request, redirect, make_response, render_template
 
 app = Flask(__name__)
+check = False
+top = 5
 
 @app.route("/")
 def home():
@@ -16,7 +18,7 @@ def home():
 def myIP():
     ip_user = request.cookies.get('ip_user')
     
-    return render_template('myip.html', ip_user=ip_user)
+    return render_template('myip.html', ip_user=ip_user, top=top, check=check)
 
 # NOTAS
 # Se declará el nombre de la app
@@ -24,3 +26,8 @@ def myIP():
 # Con make_response, se generá una respuesta a cliente que lo redireccióna al path indicado.
 # Se le genera una cookie al cliente que almacena su dirección IP.
 # Tras la redirección se obtiene la info de la cookie y se imprime en pantalla.
+# Se agrega render_template (path por defecto ./template).
+# Para agregar código de python en los archivos html se utilizan las estructuras de control.
+# en myip.html, se agregan dos estructuras de control, condicional (if) y ciclica (for) 
+
+
