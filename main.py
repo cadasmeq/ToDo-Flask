12ddabc62e81d5d15c373614517fc9a01b8014ba
cadasmeq (linux) <cadasmeq02@gmail.com>
2020-07-28 15:22:19 -0400

    Fixed path order of sandbox

--- main.py
+++ main.py
@@ -1,42 +1,40 @@
-from flask import Flask, request, make_response, redirect, render_template
+from flask import Flask, request, redirect, make_response, render_template
 
-app = Flask(__name__)           # Crea instancia de Flask, cuyo parametro __name__ (nombre del archivo) será el nombre de la app.
+app = Flask(__name__)
+check = True
+top = 5
 
-todos = ['Todo 1', 'Todo 2', 'Todo 3', 'Todo 4']
+params = {}
 
-@app.route('/') # Ruta donde se correrá esta función. Es decir, Cuando el buscador haga una petición a nuestro servidor, va a llegar a ruta raiz.
-def index():
-    user_ip = request.remote_addr   #obtiene la ip de la conexión remota
-    make_redir = redirect("/hello")
+@app.route("/")
+def home():
+    remote_ip = request.remote_addr
+    make_redir  = redirect("/myIP")
     response = make_response(make_redir)
-    response.set_cookie('user_ip', user_ip)
+    response.set_cookie("ip_user", remote_ip)
 
     return response
+    
 
-
-@app.route('/hello')    
-def hello():
-    user_ip = request.cookies.get('user_ip')
-    context = {
-        'user_ip':user_ip, 
-        'todos':todos,
+@app.route('/myIP')
+def myIP():
+    ip_user = request.cookies.get('ip_user')
+    params = {
+        "ip_user":ip_user,
+        "check":check,
+        "top":top,
     }
     
-    return render_template('hello.html', **context)     # expande el diccionario para pasar cada una de las variables
-   
+    return render_template('myip.html', **params)
 
+# NOTAS
+# Se declará el nombre de la app
+# Utilizando request, se le pide la IP al cliente web.
+# Con make_response, se generá una respuesta a cliente que lo redireccióna al path indicado.
+# Se le genera una cookie al cliente que almacena su dirección IP.
+# Tras la redirección se obtiene la info de la cookie y se imprime en pantalla.
+# Se agrega render_template (path por defecto ./template).
+# Para agregar código de python en los archivos html se utilizan las estructuras de control.
+# en myip.html, se agregan dos estructuras de control, condicional (if) y ciclica (for) 
+# Se agregan a diccionario algunos parametros del render_template y al usar **params, se expande diccionarió completo.
 
-
-
-# \venv\Scripts\activate
-# set FLASK_APP=main.py     -> establece la variable FLASK_APP a main.py
-# set FLASK_DEBUG=1         -> establece la variable FLASK_DEBUGG a 1 para habilitar debug mode
-# flas run
-
-# Estructuras de control
-# 1.- detectar ip
-# 2.- si, regresamos mensaje
-# 3.- si no, crear link que vuelva  a raiz y asi obtener ip y devolvernos a hello
-#
-#
-# 2.- Crear una variable de lista e iterar en cada string.
