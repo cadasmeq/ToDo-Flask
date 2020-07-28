af419e997b3fe36e197f0be0f6a5252cf1fa6763
cadasmeq (linux) <cadasmeq02@gmail.com>
2020-07-28 12:35:44 -0400

    Modified response parameters as a dicctionary

--- practice_sandbox/main.py
+++ practice_sandbox/main.py
@@ -1,8 +1,10 @@
 from flask import Flask, request, redirect, make_response, render_template
 
 app = Flask(__name__)
-check = False
+check = True
 top = 5
+
+params = {}
 
 @app.route("/")
 def home():
@@ -17,8 +19,13 @@ def home():
 @app.route('/myIP')
 def myIP():
     ip_user = request.cookies.get('ip_user')
+    params = {
+        "ip_user":ip_user,
+        "check":check,
+        "top":top,
+    }
     
-    return render_template('myip.html', ip_user=ip_user, top=top, check=check)
+    return render_template('myip.html', **params)
 
 # NOTAS
 # Se declará el nombre de la app
@@ -29,5 +36,5 @@ def myIP():
 # Se agrega render_template (path por defecto ./template).
 # Para agregar código de python en los archivos html se utilizan las estructuras de control.
 # en myip.html, se agregan dos estructuras de control, condicional (if) y ciclica (for) 
-
+# Se agregan a diccionario algunos parametros del render_template y al usar **params, se expande diccionarió completo.
 
