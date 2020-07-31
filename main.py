cf0bfe9dcfb538a13a0f7de9735b1d12eb01d804
cris-linux <cadasmeq02@gmail.com>
2020-07-31 13:33:28 -0400

    modifications in flask_notes

---
+++ main.py
@@ -0,0 +1,44 @@
+from flask import Flask, request, redirect, make_response, render_template
+
+app = Flask(__name__)
+check = True
+todos = ["Limpiar la mesa", "Hacer ejercicio", "Leer un libro"]
+params = {}
+
+@app.route("/") #home?
+def home():
+    remote_ip = request.remote_addr
+    make_redir  = redirect("/myIP")
+    response = make_response(make_redir)
+    response.set_cookie("ip_user", remote_ip)
+
+    return response
+    
+
+@app.route('/myIP')
+def myIP():
+    ip_user = request.cookies.get('ip_user')
+    params = {
+        "ip_user":ip_user,
+        "todos":todos,
+        'check':check
+    }
+    
+    return render_template('myip.html', **params)
+
+@app.errorhandler(404)
+def not_found(error):
+    return render_template('404.html', error=error)
+
+# NOTAS
+# Se declará el nombre de la app
+# Utilizando request, se le pide la IP al cliente web.
+# Con make_response, se generá una respuesta a cliente que lo redireccióna al path indicado.
+# Se le genera una cookie al cliente que almacena su dirección IP.
+# Tras la redirección se obtiene la info de la cookie y se imprime en pantalla.
+# Se agrega render_template (path por defecto ./template).
+# Para agregar código de python en los archivos html se utilizan las estructuras de control.
+# en myip.html, se agregan dos estructuras de control, condicional (if) y ciclica (for) 
+# Se agregan a diccionario algunos parametros del render_template y al usar **params, se expande diccionarió completo.
+
+# Se agrega check al diccionario.
