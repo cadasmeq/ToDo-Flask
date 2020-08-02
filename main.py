c690bd12d400248ff6bf464c50de30d28235dc43
cris-linux <cadasmeq02@gmail.com>
2020-08-02 17:05:10 -0400

    Changed cookies for session

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
-from flask import Flask, request, redirect, make_response, render_template
+from flask import Flask, request, redirect, make_response, render_template, session
 from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
 check = True
 todos = ["Limpiar la mesa", "Hacer ejercicio", "Leer un libro"]
+app.config['SECRET_KEY'] = "THIS_IS_THE_KEY"
 params = {}
 
 @app.route("/") #home?
@@ -12,14 +13,16 @@ def home():
     remote_ip = request.remote_addr
     make_redir  = redirect("/myIP")
     response = make_response(make_redir)
-    response.set_cookie("ip_user", remote_ip)
+    session['ip_user'] = remote_ip
+    # response.set_cookie("ip_user", remote_ip)
 
     return response
     
 
 @app.route('/myIP')
 def myIP():
-    ip_user = request.cookies.get('ip_user')
+    #ip_user = request.cookies.get('ip_user')
+    ip_user = session.get('ip_user')
     params = {
         "ip_user":ip_user,
         "todos":todos,
