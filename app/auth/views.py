cfa9aef009fed3efaef9d73559da22b0fcdcb5fe
cris-linux <cadasmeq02@gmail.com>
2020-08-07 10:23:31 -0400

    Implemented login_manager and fixed login user method

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,20 +1,42 @@
 from flask import render_template, session, flash, redirect, url_for
 from app.forms import LoginForm
 from . import auth
+from app.firestore_service import get_user
+from app.models import UserModel, UserData
+from flask_login import login_user
 
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
-
     login_form = LoginForm()
     context = {
-        'login_form':login_form
+        'login_form': login_form
     }
-    
+
     if login_form.validate_on_submit():
         username = login_form.username.data
-        session['username'] = username
-        flash("Usuario registrado con exito.")
+        password = login_form.password.data
 
-        return redirect(url_for('welcome'))
+        user_doc = get_user(username)
+
+        if user_doc.to_dict() is not None:
+            password_from_db = user_doc.to_dict()['password']
+
+            print(user_doc.to_dict())
+            print(user_doc.to_dict()['password'])
+
+            if password == password_from_db:
+                user_data = UserData(username, password)
+                user = UserModel(user_data)
+                login_user(user)
+
+                flash('Bienvenido de nuevo')
+
+                redirect(url_for('welcome'))
+            else:
+                flash('La informaición no coincide')
+        else:
+            flash('El usario no existe')
+
+        return redirect(url_for('home'))
 
     return render_template('login.html', **context)
--- main.py
+++ main.py
@@ -1,47 +1,20 @@
-from flask import Flask, request, redirect, make_response, render_template, session, url_for, flash
 import unittest
+from flask import Flask, request, redirect, make_response, render_template, session, url_for, flash
+from flask_login import login_required, current_user
+
 from app import create_app
 from app.forms import LoginForm
 from app.firestore_service import get_users, get_todos
+
 
 app = create_app()
 
 params = {}
 
-todos = ["Implementar POST", "Hacer ejercicio", "Leer un libro"]
-
 @app.cli.command()
 def test():
     tests = unittest.TestLoader().discover('test')
     unittest.TextTestRunner().run(tests)
-
-@app.route("/")
-def home():
-    remote_ip = request.remote_addr
-    make_redir  = redirect("/auth/login")
-    response = make_response(make_redir)
-    session['ip_user'] = remote_ip
-
-    return response
-    
-@app.route('/welcome')
-def welcome():
-
-    ip_user = session.get('ip_user')
-    username = session.get('username')
-
-    params = {
-        "ip_user":ip_user,
-        "todos": get_todos(username),
-        'username':username,
-    }
-
-    users = get_users()
-    for user in users:
-        print(user.id)
-        print(user.to_dict())
-
-    return render_template('welcome.html', **params)
 
 @app.errorhandler(404)
 def not_found(error):
@@ -50,3 +23,25 @@ def not_found(error):
 app.errorhandler(500)
 def server_error(error):
     return render_template('500.html', error=error)
+
+@app.route("/")
+def home():
+    remote_ip = request.remote_addr
+    response = make_response(redirect("welcome"))
+    session['ip_user'] = remote_ip
+
+    return response
+
+@app.route('/welcome')
+@login_required
+def welcome():
+    ip_user = session.get('ip_user')
+    username = current_user.id
+
+    params = {
+        "ip_user":ip_user,
+        "todos": get_todos(username),
+        'username':username,
+    }
+
+    return render_template('welcome.html', **params)
